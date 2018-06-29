5df4a197e54431e2daab856cf48b83db46ac69e6
pratikmts <pratik.d@mythemeshop.com>
2018-06-29 17:33:38 +0530

    v1.1.6 (Changed Admin notices)
    Changed admin notices

--- includes/functions-general.php
+++ includes/functions-general.php
@@ -574,17 +574,34 @@ function wp_real_estate_admin_notice() {
 			printf(__('<p><strong>WP Real Estate Pro</strong> offers advanced IDX integration, Paid Listings, User Subscriptions, Related Listings, Import/Export, Payment Gateways and much more... <br><a target="_blank" href="https://mythemeshop.com/plugins/wp-real-estate-pro/?utm_source=WP+Real+Estate&utm_medium=Notification+Link&utm_content=WP+Real+Estate+Pro+LP&utm_campaign=WordPressOrg"><strong>Grab your copy now!</strong></a></p><a class="notice-dismiss" href="%1$s"></a>'), '?wp_real_estate_admin_notice_ignore=0');
 			echo "</div>";
     }
+
+    /* Other notice appears right after activating */
+		/* And it gets hidden after showing 3 times */
+		if ( ! get_user_meta($user_id, 'wp_real_estate_ignore_notice_2') && get_option('wp_real_estate_ignore_notice_views', 0) < 3 && get_option( 'wp_real_estate_activated', 0 ) ) {
+			$views = get_option('wp_real_estate_notice_views', 0);
+			update_option( 'wp_real_estate_notice_views', ($views + 1) );
+			echo '<div class="updated notice-info wp-real-estate-notice" id="wprealestate-notice2" style="position:relative;">';
+			echo '<p>';
+			_e('Thank you for trying WP Real Estate. We hope you will like it.', 'wp-real-estate');
+			echo '</p>';
+			echo '<a class="notice-dismiss" href="?wp_real_estate_admin_notice_ignore=1"></a>';
+			echo "</div>";
+		}
 }
 
 add_action('admin_init', 'wp_real_estate_admin_notice_ignore');
 
 function wp_real_estate_admin_notice_ignore() {
-    global $current_user;
-        $user_id = $current_user->ID;
-        /* If user clicks to ignore the notice, add that to their user meta */
-        if ( isset($_GET['wp_real_estate_admin_notice_ignore']) && '0' == $_GET['wp_real_estate_admin_notice_ignore'] ) {
-             add_user_meta($user_id, 'wp_real_estate_ignore_notice', 'true', true);
-    }
+	global $current_user;
+	$user_id = $current_user->ID;
+	/* If user clicks to ignore the notice, add that to their user meta */
+	if ( isset($_GET['wp_real_estate_admin_notice_ignore']) ) {
+		if('0' == $_GET['wp_real_estate_admin_notice_ignore']) {
+			add_user_meta($user_id, 'wp_real_estate_ignore_notice', 'true', true);
+		} else if('1' == $_GET['wp_real_estate_admin_notice_ignore']) {
+			add_user_meta($user_id, 'wp_real_estate_ignore_notice_2', 'true', true);
+		}
+	}
 }
 
 function wre_check_image_file( $file ) {
